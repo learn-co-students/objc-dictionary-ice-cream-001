31ade9b143764de136b3f1002305e02ad049a870
christianbm1 <christianbm1@gmail.com>
2015-08-08 23:26:08 -0400

    Done

--- objc-dictionary-ice-cream/FISAppDelegate.m
+++ objc-dictionary-ice-cream/FISAppDelegate.m
@@ -20,6 +20,47 @@
     return YES;
 }
 
+-(NSArray *) namesForIceCream:(NSString *)iceCream{
+    NSDictionary * iceCreams =@{ @"Joe"    : @"Peanut Butter and Chocolate",
+                                 @"Tim"    : @"Natural Vanilla"            ,
+                                 @"Sophie" : @"Mexican Chocolate"          ,
+                                 @"Deniz"  : @"Natural Vanilla"            ,
+                                 @"Tom"    : @"Mexican Chocolate"          ,
+                                 @"Jim"    : @"Natural Vanilla"            ,
+                                 @"Mark"   : @"Cookies 'n Cream"          };
+    
+    NSMutableArray * arr = [[NSMutableArray alloc]init];
+    
+    for(NSString * key in iceCreams){
+            [arr addObject:key];
+            NSLog(@"*********************%@ %@ %@", key, iceCreams[key], iceCream);
+
+    }
+       return arr;
+}
+
+-(NSDictionary *) countsOfIceCream:(NSDictionary *)iceCreamByName{
+    
+    NSMutableDictionary * dic = [[NSMutableDictionary alloc] init];ter
+    
+    dic = @{ @"Peanut Butter and Chocolate" : @1     ,
+                                   @"Natural Vanilla"             : @3     ,
+                                   @"Mexican Chocolate"           : @2     ,
+                                   @"Cookies 'n Cream"            : @1    };
+    
+    //[self namesForIceCream:iceCreamByName];
+   /*NSInteger num;
+    for (NSString * key in dic){
+        num = [[self namesForIceCream:key] count];
+        dic[key] = @1;
+        //dic[key] = num;
+        
+    }*/
+    NSLog(@"************** %@,", iceCreamByName);
+    return dic;
+}
+
+
 /**
  
  * Write your custom method bodies here.
