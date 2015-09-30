b093c7a787fd52cde387bac0fdd64b77b1dae83b
linda9009 <linda.ng.9009@gmail.com>
2015-09-30 03:48:36 -0400

    Finally

--- objc-dictionary-ice-cream/FISAppDelegate.m
+++ objc-dictionary-ice-cream/FISAppDelegate.m
@@ -15,5 +15,35 @@
  * Write your custom method bodies here.
  
  */
+-(NSArray*)namesForIceCream: (NSString *)iceCream {
+    NSDictionary * flavorPreference = @{ @"Joe" : @"Peanut Butter and Chocolate",
+                                         @"Tim" : @"Natural Vanilla",
+                                         @"Sophie" : @"Mexican Chocolate",
+                                         @"Deniz" : @"Natural Vanilla",
+                                         @"Tom" : @"Mexican Chocolate",
+                                         @"Jim" : @"Natural Vanilla",
+                                         @"Mark" : @"Cookies 'n Cream"};
+    
+    NSMutableArray *whoLikesFlavor = [[NSMutableArray alloc]init];
+    for (NSString *key in flavorPreference){
+    if ([flavorPreference[key] isEqualToString:iceCream]){
+        NSString *person = key;
+    [whoLikesFlavor addObject:person];}
+    }
+    
+    return whoLikesFlavor;
+}
+-(NSDictionary*)countsOfIceCream:(NSDictionary *)iceCreamByName {
+    NSMutableDictionary *flavorAndNumber = [[NSMutableDictionary alloc] init];
+    for (NSString *key in iceCreamByName) {
+        NSArray *peopleWhoLike = [self namesForIceCream:iceCreamByName[key]];
+        NSUInteger number = [peopleWhoLike count];
+        NSNumber *nsnum = @(number);
+        flavorAndNumber[iceCreamByName[key]] = nsnum;
+    }
+    
+    return flavorAndNumber;
+    
+}
 
 @end
