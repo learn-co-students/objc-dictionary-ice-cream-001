80f751087aa65f74a68ab86b89f5f8fd00aa2391
johnss518 <stacy.m.johnson@gmail.com>
2015-10-16 15:44:27 -0400

    implement ice cream dictionary

--- objc-dictionary-ice-cream/FISAppDelegate.m
+++ objc-dictionary-ice-cream/FISAppDelegate.m
@@ -10,10 +10,36 @@
     return YES;
 }
 
-/**
- 
- * Write your custom method bodies here.
- 
- */
+-(NSArray *)namesForIceCream:(NSString *)iceCream {
+    NSDictionary *iceCreamPreferences = @{@"Joe" : @"Peanut Butter and Chocolate",
+                                          @"Tim"  : @"Natural Vanilla",
+                                          @"Sophie" : @"Mexican Chocolate",
+                                          @"Deniz" : @"Natural Vanilla",
+                                          @"Tom" : @"Mexican Chocolate",
+                                          @"Jim" : @"Natural Vanilla",
+                                          @"Mark" : @"Cookies 'n Cream" };
+    
+    NSMutableArray *peopleWhoLikeFlavor = [[NSMutableArray alloc] init];
+    
+    for (NSString *name in iceCreamPreferences) {
+        if ([iceCream isEqualToString:iceCreamPreferences[name]]) {
+            [peopleWhoLikeFlavor addObject:name];
+        }
+    }
+             
+    return peopleWhoLikeFlavor;
+}
+
+-(NSDictionary *)countsOfIceCream:(NSDictionary *)iceCreamByName {
+    NSMutableDictionary *countOfIceCreamFlavor = [[NSMutableDictionary alloc] init];
+    
+    for (NSString *name in iceCreamByName) {
+        NSString *iceCreamFlavor = iceCreamByName[name];
+        NSUInteger count = [[self namesForIceCream:iceCreamFlavor] count];
+        countOfIceCreamFlavor[iceCreamFlavor] = [NSNumber numberWithUnsignedInteger:count];
+    }
+    
+    return countOfIceCreamFlavor;
+}
 
 @end
