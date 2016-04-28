6ac0cd137ad68186af4b4881aa45010993319f45
John Richardson <richardsonjm@gmail.com>
2016-04-28 16:48:48 -0400

    First 3 tests passing

--- objc-dictionary-ice-cream/FISAppDelegate.m
+++ objc-dictionary-ice-cream/FISAppDelegate.m
@@ -10,10 +10,27 @@
     return YES;
 }
 
-/**
- 
- * Write your custom method bodies here.
- 
- */
+-(NSArray *)namesForIceCream:(NSString *)icecream {
+    NSDictionary *iceCreamByName = @{ @"Joe"    : @"Peanut Butter and Chocolate",
+                        @"Tim"    : @"Natural Vanilla"            ,
+                        @"Sophie" : @"Mexican Chocolate"          ,
+                        @"Deniz"  : @"Natural Vanilla"            ,
+                        @"Tom"    : @"Mexican Chocolate"          ,
+                        @"Jim"    : @"Natural Vanilla"            ,
+                        @"Mark"   : @"Cookies 'n Cream"          };
+    NSMutableArray *result = [[NSMutableArray alloc] init];
+    
+    for(NSString *key in iceCreamByName) {
+        if ([iceCreamByName[key] isEqualToString:icecream]) {
+            [result addObject:key];
+        }
+    }
+    return result;
+}
+
+-(NSDictionary *)countsOfIceCream:(NSDictionary *)iceCreamByName {
+    
+    return nil;
+}
 
 @end
